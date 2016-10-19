dff6c1082ea08e58ad2bc54235cf4eb1060389a3
Deepak Jois <deepak.jois@gmail.com>
2016-10-19 21:59:43 +0530

    Add text_to_paragraph function to ufy module.

--- src/ufy/init.lua
+++ src/ufy/init.lua
@@ -1,3 +1,4 @@
+local utf8 = require("compat53.utf8")
 local ufy = {}
 
 -- Copied from: http://tex.stackexchange.com/questions/218855/using-lualatex-and-sqlite3/219228#219228
@@ -50,5 +51,58 @@ function ufy.init()
   pdf.setpkresolution(600)
   pdf.setminorversion(5)
 end
+
+-- build paragraph node
+-- adapted from: http://tex.stackexchange.com/questions/114568/can-i-create-a-node-list-from-some-text-entirely-within-lua
+function ufy.text_to_paragraph(text)
+  local current_font = font.current()
+  local font_params = font.getfont(current_font).parameters
+
+  local para_head = node.new("local_par")
+  para_head.dir = "TLT"
+
+  local last = para_head
+
+  local indent = node.new("hlist",3)
+  indent.width = tex.parindent
+  last.next = indent
+  last = indent
+
+  for p,v in utf8.codes(text) do
+    local n
+    if v < 32 then
+      goto skipchar
+    elseif v == 32 then -- FIXME use Unicode properties to identify whitespace
+      n = node.new("glue",13)
+      node.setglue(n, font_params.space, font_params.space_shrink, font_params.space_stretch)
+    else
+      n = node.new("glyph", 1)
+      n.font = current_font
+      n.char = v
+      n.lang = tex.language
+      n.uchyph = 1
+      n.left = tex.lefthyphenmin
+      n.right = tex.righthyphenmin
+    end
+    last.next = n
+    last = n
+    ::skipchar::
+  end
+
+  -- now add the final parts: a penalty and the parfillskip glue
+  local penalty = node.new("penalty", 0)
+  penalty.penalty = 10000
+
+  local parfillskip = node.new("glue", 14)
+  parfillskip.stretch = 2^16
+  parfillskip.stretch_order = 2
+
+  last.next = penalty
+  penalty.next = parfillskip
+
+  node.slide(para_head)
+  return para_head
+end
+
 
 return ufy
