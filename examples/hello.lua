811b4d39f12599fec7bd2debcf6c05162b9c79c2
Deepak Jois <deepak.jois@gmail.com>
2016-10-19 16:20:51 +0530

    Add first example

---
+++ examples/hello.lua
@@ -0,0 +1,69 @@
+local utf8 = require("compat53.utf8")
+
+-- build paragraph node
+-- adapted from: http://tex.stackexchange.com/questions/114568/can-i-create-a-node-list-from-some-text-entirely-within-lua
+local function text_to_paragraph(text)
+  local current_font = font.current()
+  local font_params = font.getfont(current_font).parameters
+
+  local para_head = node.new("local_par")
+  para_head.dir = "TLT"
+
+  local last = para_head
+
+  local indent = node.new("hlist",3)
+  indent.width = tex.parindent
+  last.next = indent
+  last = indent
+
+  for p,v in utf8.codes(text) do
+    local n
+    if v == 32 then -- FIXME use Unicode properties to identify whitespace
+      n = node.new("glue",13)
+      node.setglue(n, font_params.space, font_params.space_shrink, font_params.space_stretch)
+    else
+      n = node.new("glyph", 1)
+      n.font = current_font
+      n.char = v
+      n.lang = tex.language
+      n.uchyph = 1
+      n.left = tex.lefthyphenmin
+      n.right = tex.righthyphenmin
+    end
+    last.next = n
+    last = n
+  end
+
+  -- now add the final parts: a penalty and the parfillskip glue
+  local penalty = node.new("penalty", 0)
+  penalty.penalty = 10000
+
+  local parfillskip = node.new("glue", 14)
+  parfillskip.stretch = 2^16
+  parfillskip.stretch_order = 2
+
+  last.next = penalty
+  penalty.next = parfillskip
+
+  node.slide(para_head)
+  return para_head
+end
+
+-- Page settings
+tex.pagewidth = "210mm"
+tex.pageheight = "297mm"
+tex.hsize = "210mm"
+
+-- Set the paragraph indentation
+tex.parindent = "50pt"
+
+-- Convert text to nodes
+local text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+local head = text_to_paragraph(text)
+
+-- Break the paragraph into vertically stacked boxes
+local vbox = tex.linebreak(head, { hsize = tex.hsize })
+
+-- Write node to ‘current’ list
+node.write(vbox)
+
