7e0972e09921a20bed62745257e4e51de2a1dcef
Deepak Jois <deepak.jois@gmail.com>
2017-05-25 18:21:55 +0530

    new loader

--- src/ufy/loader.lua
+++ src/ufy/loader.lua
@@ -1,49 +1,66 @@
 local loader = {}
 
--- Copied from: http://tex.stackexchange.com/questions/218855/using-lualatex-and-sqlite3/219228#219228
-local function make_loader(ppath, pos, loadfunc)
-  local default_loader = package.searchers[pos]
-  local ldr = function(name)
-    local file, _ = package.searchpath(name,ppath)
-    if not file then
-      local msg = "\n\t[lualoader] Search failed"
-      local ret = default_loader(name)
-      if type(ret) == "string" then
-        return msg ..ret
-      elseif type(ret) == "nil" then
-        return msg
-      else
-        return ret
-      end
-    end
-    local ldr,err = loadfunc(file,name)
-    if not ldr then
-      return "\n\t[lualoader] Loading error:\n\t"..err
-    end
-    return ldr
+-- Cache the current kpse based searchers
+local kpse_lua_searcher = package.searchers[2]
+local kpse_clua_searcher = package.searchers[3]
+
+-- Emulates the default package.searchers[2] searcher.
+local function lua_searcher(name)
+  local file, err = package.searchpath(name,package.path)
+  if err then
+    return err
+  else
+    return loadfile(f)
   end
-  package.searchers[pos] = ldr
 end
 
-local function binary_loader(file, name)
-  local symbol = name:gsub("%.","_")
-  return package.loadlib(file, "luaopen_"..symbol)
+-- Emulates the default package.searchers[3] searcher.
+local function clua_searcher(name)
+  local file, err = package.searchpath(name, package.cpath)
+  if err then
+    return err
+  else
+    local symbol = name:gsub("%.","_")
+    return package.loadlib(f, "luaopen_"..symbol)
+  end
 end
 
--- Revert the package searchers to use package.path and package.cpath.
+local function combine_searchers(searcher1, searcher2, identifier)
+  return function(name)
+    print("Using "..identifier)
+    print("Looking for "..name)
+    -- First look under the default searcher
+    local loader1, file1 = searcher1(name)
+    print(loader1)
+    if type(loader1) == "string" and not file1 then -- Not found using searcher1. Try searcher2
+      print("Looking again for "..name)
+      local loader2, file2 = searcher2(name)
+      if not file2 then return string.format("\n\t%s\n\t[%s] %s\n\t",loader1, identifier,loader2) end
+      return loader2, file2
+    end
+    return loader1, file1
+  end
+end
+
+-- Revert the package searchers to use package.path and package.cpath
+-- in case a module is not found.
 --
 -- Package searching logic is overridden by default in LuaTeX to use kpse.
 -- Calling this function reverts the searchers to use package.path and
--- package.cpath first, failing which it will try the kpse based searcher.
+-- package.cpath, if the kpse based searcher is not able to locate
+-- a module.
 --
 -- Package Loading References:
 -- 1. http://www.lua.org/manual/5.2/manual.html#pdf-package.searchers
 -- 2. LuaTeX Manual, Section 3.2, Lua behavior
 function loader.revert_package_searchers()
-  make_loader(package.path,2,function(file)
-    return loadfile(file)
-  end)
-  make_loader(package.cpath,3, binary_loader)
+  package.searchers[2] = combine_searchers(kpse_lua_searcher, lua_searcher, "lualoader")
+  package.searchers[3] = combine_searchers(kpse_clua_searcher, clua_searcher, "lua C loader")
+end
+
+function loader.restore_kpse_searchers()
+  package.searchers[2] = kpse_lua_searcher
+  package.searchers[3] = kpse_clua_searcher
 end
 
 return loader
