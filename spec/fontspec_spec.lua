aa457bad642d337de6a3de75b10e295438342c1c
Deepak Jois <deepak.jois@gmail.com>
2017-06-08 16:46:23 +0530

    Update specs

--- spec/fontspec_spec.lua
+++ spec/fontspec_spec.lua
@@ -15,17 +15,18 @@ local function t(spec, result)
 end
 
 test_cases = {
-  t("[lmroman10-regular]",                       {filename = "lmroman10-regular"}),
-  t("[abc.ttc:1]",                               {filename = "abc.ttc", fontindex = "1"}),
-  t("Liberation Serif",                          {fontname = "Liberation Serif"}),
-  t("Liberation Serif /B",                       {fontname = "Liberation Serif", options = {bold = true}}),
-  t("Liberation Serif /BI /GR",                  {fontname = "Liberation Serif", options = {bolditalic = true, graphite = true}}),
-  t("Liberation Serif /U",                       {fontname = "Liberation Serif"}), -- ignore unknown options
-  t("[abc.ttf]:+smcp",                           {filename = "abc.ttf", features = {smcp = true}}),
-  t("Liberation Serif:+smcp",                    {fontname = "Liberation Serif", features = {smcp = true}}),
-  t("[abc.ttf]:+smcp,+aalt=1",                   {filename = "abc.ttf", features = {smcp = true, aalt = "1"}}),
-  t("[abc.ttf]:-smcp, +aalt=1",                  {filename = "abc.ttf", features = {smcp = false, aalt = "1"}}),
-  t("[abc.ttf]:script=arab, lang=urd",           {filename = "abc.ttf", features = {script = "arab", lang = "urd"}}),
-  t("[abc.ttf:script=arab, lang=urd",            nil),
-  t("[abc.ttf:script%arab, lang=urd",            nil)
+  t("[lmroman10-regular]",             {filename = "lmroman10-regular"}),
+  t("[abc.ttc:1]",                     {filename = "abc.ttc", fontindex = "1"}),
+  t("Liberation Serif",                {fontname = "Liberation Serif"}),
+  t("Liberation Serif /B",             {fontname = "Liberation Serif", options = {bold = true}}),
+  t("Liberation Serif /BI/GR",         {fontname = "Liberation Serif", options = {bolditalic = true, graphite = true}}),
+  t("Liberation Serif /U:+smcp",       {fontname = "Liberation Serif", features = {smcp = true}}), -- ignore unknown options
+  t("Liberation Serif /S=23",          nil), -- FIXME currently not supported
+  t("[abc.ttf]:+smcp",                 {filename = "abc.ttf", features = {smcp = true}}),
+  t("Liberation Serif:+smcp",          {fontname = "Liberation Serif", features = {smcp = true}}),
+  t("[abc.ttf]:+smcp,+aalt=1",         {filename = "abc.ttf", features = {smcp = true, aalt = "1"}}),
+  t("[abc.ttf]:-smcp, +aalt=1",        {filename = "abc.ttf", features = {smcp = false, aalt = "1"}}),
+  t("[abc.ttf]:script=arab, lang=urd", {filename = "abc.ttf", features = {script = "arab", lang = "urd"}}),
+  t("[abc.ttf:script=arab, lang=urd",  nil),
+  t("[abc.ttf:script%arab, lang=urd",  nil)
 }
